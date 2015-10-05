ced16f689cbbacf097ee20a55479a9c4c5ec1eda
capraynor <732382150@qq.com>
2015-10-05 10:22:13 +0800

    增加：使用CRUD 未完成：数据库读写

--- ResponseWithJson/routes/students.js
+++ ResponseWithJson/routes/students.js
@@ -9,7 +9,31 @@ router.get('/class/:class/name/:name', function (req, res, next) {
         class:req.params.class
     });
 
-    console.log(req.params);
 }); // get post put delete 方法均可以在这里使用。
+
+router.post('/class/:class/name/:name', function (req, res, next) {
+    res.json({
+        name:req.params.name,
+        class:req.params.class
+    });
+
+});
+
+router.put('/class/:class/name/:name', function (req, res, next) {
+    res.json({
+        name:req.params.name,
+        class:req.params.class
+    });
+
+});
+
+
+
+router.delete('/class/:class/name/:name', function (req, res, next) {
+    res.json({
+        name:req.params.name,
+        class:req.params.class
+    });
+});
 
 module.exports = router;
